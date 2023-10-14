be58b73dd8c29fb85737bd18f79a4e8bc6fb001d
GeNT! <98806559+Gentritbytyqi@users.noreply.github.com>
2023-10-14 18:34:56 +0200

    Add files via upload

---
+++ includes/connect.php
@@ -0,0 +1,13 @@
+<?php
+$con=mysqli_connect('localhost:3306','root','','mystore');
+if(!$con){
+    die(mysqli_connect_error($con));
+}
+// $con2 = mysqli_connect('127.0.0.1:3307', 'root@localhost', '', 'user_db');
+// if (!$con2) {
+//     die(mysqli_connect_error($con2));
+// }
+
+?>
+
+  
